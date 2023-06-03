fdc16399d612503d726120305ee89a3b35ff36eb
NapoliN <sakashita.y.aa@m.titech.ac.jp>
2023-06-04 01:51:05 +0900

    css調整

--- src/component/ArtCard.tsx
+++ src/component/ArtCard.tsx
@@ -1,17 +1,17 @@
 import { Card } from "react-bootstrap"
 
 type ArtCardType = {
-    src : string,
-    title : string,
-    createdAt : string
-    artWidth : number
-    description? : string
+    src: string,
+    title: string,
+    createdAt: string
+    artWidth: number
+    description?: string
 }
 
 const ArtCard = (info: ArtCardType) => {
     return (
-        <Card style={{width: info.artWidth + "px"}} className="p-0">
-            <Card.Img src={info.src}/>
+        <Card style={{ width: info.artWidth + "px" }} className="mx-auto my-2">
+            <Card.Img src={info.src} />
             <Card.Title> {info.title} </Card.Title>
             {
                 info.description != undefined &&
