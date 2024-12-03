f7544734564847035ae19ef79176f04e72aea723
NapoliN <sakashita.y.aa@m.titech.ac.jp>
2024-12-04 01:47:09 +0900

    mathjaxを入れた

--- src/pages/Markdown.tsx
+++ src/pages/Markdown.tsx
@@ -1,11 +1,24 @@
 import { ReactComponent as Markdown } from '../notes/helloworld.md';
+import { MathJax3Config, MathJaxContext } from 'better-react-mathjax';
+
+const config: MathJax3Config = {
+    tex:{
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        //processEscapes: true,
+        //processEnvironments: true,
+        //packages: {'[+]': ['ams', 'newcommand', 'configMacros']}
+    }
+};
 
 
 const MarkdownViewer = () => {
   return (
-    <div className="markdown-body">
-        <Markdown />
-    </div>
+    <MathJaxContext config={config}>
+        <div className="markdown-body">
+            <Markdown />
+        </div>
+    </MathJaxContext>
   );
 };
 
