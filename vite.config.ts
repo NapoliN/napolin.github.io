889385b5750f92e3096d1937b81b5fcb3a0187da
NapoliN <sakashita.y.aa@m.titech.ac.jp>
2024-12-08 16:23:51 +0900

    add contaienr-support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { plugin as markdown, PluginOptions as MdOption, Mode as MdMode } from "vite-plugin-markdown"
 import Shiki from '@shikijs/markdown-it'
 import MarkdownIt from 'markdown-it'
+import MarkdownItContainer from 'markdown-it-container'
 
 const md = MarkdownIt()
 
@@ -14,6 +15,28 @@ Shiki({
 }).then((highlighter) => {
   md.use(highlighter)
 })
+
+const containers = [
+  { type: 'info', class: 'info' },
+  { type: 'warning', class: 'warning' },
+  { type: 'error', class: 'error' },
+  { type: 'success', class: 'success' },
+];
+
+containers.forEach((container) => {
+  md.use(MarkdownItContainer, container.type, {
+    render(tokens, idx) {
+      const token = tokens[idx];
+      if (token.nesting === 1) {
+        // 開始タグ
+        return `<div class="${container.class}">\n`;
+      } else {
+        // 終了タグ
+        return '</div>\n';
+      }
+    },
+  });
+});
 
 const mdOption: MdOption = {
   mode: [MdMode.HTML],
